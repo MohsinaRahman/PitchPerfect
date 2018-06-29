7dadfaae484b9b02f95aabdadcf08b2d523e075d
Mohsina Rahman <nabila.rahman55@yahoo.com>
2018-06-29 08:09:53 -0700

    Removed memory warn function, added header.

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -2,24 +2,18 @@
 //  ViewController.swift
 //  PitchPerfect
 //
-//  Created by Nabila Reza on 6/29/18.
-//  Copyright © 2018 Nabila Reza. All rights reserved.
+//  Created by Mohsina Rahman on 6/29/18.
+//  Copyright © 2018 Mohsina Rahman. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
